288837eaf466ae5742b824df5167fd729ae92805
DevonSimmons <devosimms@gmail.com>
2022-06-03 08:46:09 -0300

    Adding logic to display API data on web page
    Added ability to display API data on front end. This data is just the base data. Not clickable or filtered.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-
+import React, { useEffect, useState } from 'react';
+import { format } from 'date-fns';
 import './App.css';
 
+interface RepoProperties {
+  name: string;
+  description: string;
+  language: string;
+  forks: number;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  created_at: number;
+}
+
 export function App() {
+  const repoAPI = 'http://localhost:4000/repos/';
+
+  const [name, setName] = useState<RepoProperties[]>([]);
+
+  useEffect(() => {
+    repos();
+  }, []);
+
+  const repos = async () => {
+    const response = await fetch(repoAPI);
+
+    setName(await response.json());
+  };
+  //Sort array by date in reverse-chronological order
+ 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ol>
+          {name.map((data) => {
+            return (
+              <div key={data.created_at}>
+                <li key={data.created_at}>
+                  <h2>Name: {data.name}</h2>
+                  <p>Description: {data.description}</p>
+                  <p>Language: {data.language}</p>
+                  <p>Forks: {data.forks}</p>
+                  <p>Created: {data.created_at}</p>
+                </li>
+              </div>
+            );
+          })}
+        </ol>
       </header>
     </div>
   );
